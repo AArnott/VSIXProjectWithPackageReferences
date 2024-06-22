074005c8d44842a3889b19e264e4c4c0f7256fc0
Andrew Arnott <andrewarnott@live.com>
2024-06-22 07:56:26 -0600

    Modernize the project

--- VSIXProject11/ToolWindow1Package.cs
+++ VSIXProject11/ToolWindow1Package.cs
@@ -1,33 +1,28 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
-using System.Threading.Tasks;
-using Task = System.Threading.Tasks.Task;
 using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
 
 namespace VSIXProject11
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideToolWindow(typeof(ToolWindow1))]
-    [Guid(ToolWindow1Package.PackageGuidString)]
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class ToolWindow1Package : Package
-    {
-        public const string PackageGuidString = "2897fc32-a5e5-4206-8d60-7a53709eea1f";
-        protected override void Initialize()
-        {
-            ToolWindow1Command.Initialize(this);
-            base.Initialize();
-        }
-    }
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	[Guid(ToolWindow1Package.PackageGuidString)]
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+	public sealed class ToolWindow1Package : AsyncPackage
+	{
+		public const string PackageGuidString = "2897fc32-a5e5-4206-8d60-7a53709eea1f";
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			await base.InitializeAsync(cancellationToken, progress);
+
+			// When initialized asynchronously, we *may* be on a background thread at this point.
+			// Do any initialization that requires the UI thread after switching to the UI thread.
+			// Otherwise, remove the switch to the UI thread if you don't need it.
+			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+		}
+	}
 }
